02a8cd2d4a5dc16b0426619595feff9ce2805d4a
wisquimas <wisquimas@gmail.com>
2016-05-26 11:52:29 -0500

    Mejora templates

--- gafamx.php
+++ gafamx.php
@@ -7,7 +7,8 @@ global $movil;
 */
 define('ROUT_GAFA',__DIR__);
 
-define('THEMES_GAFA',basename( dirname( ABSPATH ) ).DIRECTORY_SEPARATOR.'themes');
+define('THEMES_GAFA',dirname( ABSPATH ).DIRECTORY_SEPARATOR.'themes');
+
 define('PLUGINS_GAFA',ROUT_GAFA.DIRECTORY_SEPARATOR.'plugins');
 
 /*
@@ -18,16 +19,15 @@ require_once(dirname( __FILE__ ) ."/functions/inicio.php");
 // Clases de utilidades. Importará todos los archivos .php dentro de la carpeta "util".
 foreach (glob(__DIR__ . DIRECTORY_SEPARATOR . "util" . DIRECTORY_SEPARATOR . "*.php") as $filename)
 {
-    require_once $filename;
+	require_once $filename;
 }
 /*
 ** CORRER UTILES INICIALES
 */
 add_action('get_header','iniciador_de_utiles');
 function iniciador_de_utiles(){
-    Generador_style_css::init();
+	Generador_style_css::init();
 };
-
 /*FUNCIONES LOGIN*/
 if( !function_exists('nuevo_logo') ){
 	function nuevo_logo() { ?>
@@ -134,14 +134,17 @@ get_is_mobile();
 
 
 function quitar_barra_administracion(){
-    return false;
+	return false;
 }
 add_filter( 'show_admin_bar' , 'quitar_barra_administracion');
 
 /* Revisar Admin */
+/**
+ * No esta haciendo una revision correcta del archivo
+ * @deprecated
+ */
 function sera_admin(){
 	if( is_admin() && isset( $current_user->data->ID ) ){
-		global $current_user;
 		$role = $current_user->roles;
 		if( isset( $role[0] ) && $role[0] != 'administrator'){
 			wp_redirect(get_home_url());
@@ -149,7 +152,7 @@ function sera_admin(){
 		};
 	};
 };
-add_action('admin_init','sera_admin');
+//add_action('admin_init','sera_admin');
 
 add_filter( 'wp_get_attachment_image_src', 'media_files_url',4,50 );
 function media_files_url( $image ) {
@@ -163,13 +166,19 @@ add_filter( 'send_password_change_email', 'no_enviar_password_change_email',3,50
 function no_enviar_password_change_email( $enviar, $user, $userdata ){
 	return false;
 };
-/*
-** DAMOS SOPORTE A NUESTRA CARPETA DE PLUGINS
-*/
+/**
+ * Annadimos carpeta customizada de templates
+ */
 add_action('setup_theme','preload_template_gafa');
 function preload_template_gafa(){
-    global $wp_theme_directories;
-    $wp_theme_directories[] = THEMES_GAFA;
+	global $wp_theme_directories;
+	$wp_theme_directories[] = THEMES_GAFA;
+}
+add_filter( 'template_directory_uri', 'preload_template_gafa_plantilla',10,3 );
+function preload_template_gafa_plantilla($template_dir_uri, $template, $theme_root_uri){
+
+	//    gafa(get_defined_vars());
+	return get_home_url()."/themes/{$template}/";
 }
 /*
 ** AÑADIR NUEVOS PLUGINS
@@ -177,13 +186,13 @@ function preload_template_gafa(){
 */
 //add_filter( 'all_plugins', 'annadir_plugins_gafa' );
 function annadir_plugins_gafa( $info = false ){
-    /*
+	/*
     **MAGIC FIELDS
     */
-    $mf = get_plugin_data(PLUGINS_GAFA.DIRECTORY_SEPARATOR.'magic-fields-2'.DIRECTORY_SEPARATOR.'main.php');
-    $info['magic-fields-2'] = $mf;
-    gafa($info,'todos los plugins');
-    return $info;
+	$mf = get_plugin_data(PLUGINS_GAFA.DIRECTORY_SEPARATOR.'magic-fields-2'.DIRECTORY_SEPARATOR.'main.php');
+	$info['magic-fields-2'] = $mf;
+	gafa($info,'todos los plugins');
+	return $info;
 };
 /*
 ** activacion_funciones_basicas
@@ -193,7 +202,8 @@ register_activation_hook( __FILE__, 'activacion_funciones_basicas' );
 ** MAIN DE ACTIVACION
 */
 function activacion_funciones_basicas(){
-    require_once( __DIR__.DIRECTORY_SEPARATOR.'instalacion'.DIRECTORY_SEPARATOR.'instalacion.php' );
+	/** @noinspection PhpIncludeInspection */
+	require_once( __DIR__ . DIRECTORY_SEPARATOR . 'instalacion' . DIRECTORY_SEPARATOR . 'instalacion.php' );
 }
 add_action('admin_init','funcionesArranque');
 function funcionesArranque(){
